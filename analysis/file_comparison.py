46afeed785d21963eb263a2aa152f112b5d49575
kathartic <latias320@gmail.com>
2022-11-22 10:42:39 -0800

    feat: add logging statements to analysis script
    Since lz_arithmetic takes forever, nice to have some timestamps to indicate when
    (approx) things were kicked off.

--- analysis/file_comparison.py
+++ analysis/file_comparison.py
@@ -3,14 +3,23 @@
 #
 # Usage example:
 # $ cd ~/ee274_project
+# $ TEST_DATA=test_data/kodim03.png
 # $ python analysis/file_comparison.py \
-#     -f test_data/kodim03.png \
+#     -f $TEST_DATA \
 #     -c filtered_zlib
+#
+# Since some compressors may take > 1 hour to compress, it's recommended to pipe
+# results to run this script as a background process, or pipe output to a file
+# instead (the latter is given as example here):
+#
+# $ echo \
+#    "$(python analysis/file_comparison.py -f $TEST_DATA -c lzarithmetic)" > results.txt
 
 import argparse
 import os
 from core.data_encoder_decoder import DataEncoder
 from core.data_block import DataBlock
+from datetime import datetime
 from PIL import Image
 from png_compressors.filtered_zlib import FilteredZlib
 from png_compressors.filtered_lz_arithmetic import FilteredLzArithmetic
@@ -32,15 +41,20 @@ def compare_file(filename: str, encoder_name: str):
     """Opens file and prints comparison."""
 
     file_stats = os.stat(filename)
-    print("Original PNG size: %d bytes" % file_stats.st_size)
+    print("[%s]: Original PNG size: %d bytes" %
+          (str(datetime.now()), file_stats.st_size))
 
     with Image.open(filename) as image_f:
         width, height, block = read_image(image_f)
 
+    print("[%s]: Image data has been successfully read." % str(datetime.now()))
+    print("[%s]: Beginning compression with %s." %
+          (str(datetime.now()), encoder_name))
     encoder = get_encoder(encoder_name, width, height)
     encoded = encoder.encode_block(block)
 
-    print("Compressed with %s: %d bytes" % (encoder_name, len(encoded) / 8))
+    print("[%s]: Compressed with %s: %d bytes" %
+          (str(datetime.now()), encoder_name, len(encoded) / 8))
 
 
 def create_parser():
@@ -67,4 +81,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
