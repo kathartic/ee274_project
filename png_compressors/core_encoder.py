82a7e80690e2cd04f36f6623832d53e204ca52b0
kathartic <latias320@gmail.com>
2022-12-02 15:07:17 -0800

    feat: add filter type flags to core encoder
    Let users configure if they want to encode filter type differently than PNG
    (i.e. don't just prepend it to the beginning of each scanline).
    
    Also add tests.

--- png_compressors/core_encoder.py
+++ png_compressors/core_encoder.py
@@ -2,15 +2,30 @@ import numpy as np
 from core.data_encoder_decoder import DataEncoder, DataDecoder
 from core.data_block import DataBlock
 from png_tools.png_filters import choose_filter
-from typing import List
+from typing import List, Tuple
 
 
 class CoreEncoder(DataEncoder):
-    """Extends `DataEncoder` with filtering methods."""
+    """Extends `DataEncoder` with filtering methods.
 
-    def __init__(self, width, height):
+    Attributes:
+        width: integer width of the image in pixels.
+        height: integer height of the image in pixels.
+        prepend_filter_type: boolean that controls if filter type is prepended
+            as a block or not. If true, filter type will be encoded separately
+            at the beginning instead of prepended to each scanline. 
+        debug_logs: boolean that controls print logging.
+    """
+
+    def __init__(self,
+                 width: int,
+                 height: int,
+                 prepend_filter_type: bool = False,
+                 debug_logs: bool = False):
         self.width = width
         self.height = height
+        self.prepend_filter_type = prepend_filter_type
+        self.debug_logs = debug_logs
 
     def _channelify(self, data_block: DataBlock) -> List[List[int]]:
         """Breaks input data into channels.
@@ -41,6 +56,37 @@ class CoreEncoder(DataEncoder):
                              len(chunks))
         return chunks
 
+    def _filter_channel(self,
+                        channel: List[int]) -> Tuple[np.ndarray, np.ndarray]:
+        """Produces tuple of filter_type, filtered_channel.
+
+        Instead of prepending to the scanline as PNG does, returns tuple of
+        filter_types, filtered_channels. There should be self.height entries in
+        the filter_type list.
+
+        Args:
+            channel Entries for a given channel.
+        Returns:
+            Tuple of filter_types, filtered_channels. filtered_channels is the
+            filtered version of input `channel`.
+        """
+        channel_block = np.array(channel).reshape(self.height, self.width)
+        filtered = np.ndarray(shape=(self.height, self.width))
+        filter_types = np.ndarray(shape=(self.height, ), dtype=int)
+
+        for i in range(self.height):
+            curr = channel_block[i]
+            prev = np.zeros(self.width) if i == 0 else channel_block[i - 1]
+            filter_type, filtered_line = choose_filter(curr, prev)
+            filter_types[i] = filter_type
+            filtered[i] = filtered_line
+
+        if (self.debug_logs):
+            print("Filter type counts:")
+            print(DataBlock(filter_types).get_counts())
+
+        return filter_types, filtered.flatten()
+
     def _filter_channels(self, chunks: List[List[int]]) -> List[int]:
         """Produces ndarray of filtered channels.
 
@@ -54,16 +100,14 @@ class CoreEncoder(DataEncoder):
         filtered = np.ndarray(shape=(self.height * len(chunks),
                                      self.width + 1),
                               dtype=int)
-        row_index = 0
-        for chunk in chunks:
-            shaped_chunk = np.array(chunk).reshape(self.height, self.width)
-            for r in range(self.height):
-                curr = shaped_chunk[r]
-                prev = np.zeros(self.width) if r > 0 else shaped_chunk[r - 1]
-                filter_type, filtered_line = choose_filter(curr, prev)
-                filtered[row_index][0] = filter_type
-                filtered[row_index][1:] = filtered_line
-                row_index += 1
+        for i in range(len(chunks)):
+            chunk = chunks[i]
+            start = i * self.height
+            end = (i + 1) * self.height
+            filter_types, filtered_chunk = self._filter_channel(chunk)
+            filtered[start:end, 0] = filter_types
+            filtered[start:end,
+                     1:] = filtered_chunk.reshape(self.height, self.width)
 
         return filtered.flatten().tolist()
 
@@ -87,5 +131,53 @@ class CoreDecoder(DataDecoder):
             List of reverse-filtered channels.
         """
 
-        # TODO(lueka): implement.
+        # TODO: implement.
         return []
+
+
+############################## TESTS ##############################
+
+
+def test_filter_channel():
+    test_channel = np.array([
+        [1, 2, 3],  # expect sub filter
+        [5, 5, 5],  # expect paeth filter
+        [5, 5, 5],  # expect up filter
+        [0, 1, 0],  # expect none filter here
+    ]).flatten().tolist()
+    expected_filter_types = [1, 4, 2, 0]
+    expected_filtered = np.array([
+        [1, 1, 1],
+        [4, 0, 0],
+        [0, 0, 0],
+        [0, 1, 0],
+    ]).flatten().tolist()
+
+    encoder = CoreEncoder(3, 4)
+    filter_types, filtered = encoder._filter_channel(test_channel)
+
+    np.testing.assert_array_equal(filter_types,
+                                  expected_filter_types,
+                                  err_msg="Filter types are not equal.")
+    np.testing.assert_array_equal(
+        filtered,
+        expected_filtered,
+        err_msg="Filtered channel arrays are not equal.")
+
+
+def test_filter_channels():
+    test_channel = np.array([
+        [4, 8, 25],  # expect sub
+        [2, 5, 15],  # expect avg
+    ]).flatten().tolist()
+    expected_channel = np.array([
+        [1, 4, 4, 17],
+        [3, 0, 0, 0],
+    ]).flatten().tolist()
+    expected = expected_channel + expected_channel + expected_channel
+    encoder = CoreEncoder(3, 2)
+
+    filtered = encoder._filter_channels(
+        [test_channel, test_channel, test_channel])
+
+    np.testing.assert_array_equal(np.array(filtered), np.array(expected))
